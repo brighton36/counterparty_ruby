465c3148c24d3128d84e9e027aadd53719c4497a
Chris DeRose <cderose@lemmy.derosetechnologies.com>
2014-12-13 02:32:00 +0000

    First working get_burns

--- spec/counterparty_client_spec.rb
+++ spec/counterparty_client_spec.rb
@@ -10,18 +10,20 @@ describe Counterparty::Client do
     its(:port){ should eq(4000) }
     its(:username){ should eq('rpc') }
     its(:password){ should eq('1234') }
-    its(:api_url){ should eq('http://localhost:4000/api/') }
+    its(:api_url){ should eq('http://rpc:1234@localhost:4000/api/') }
   end
 
   describe "#get_burns" do
-    it "what happens" do
-      ret = default_cp.get_burns( order_by: 'tx_hash', order_dir: 'asc', 
-        start_block: 280537, end_block: 280539 )
-      puts ret.inspect
+    subject{default_cp.get_burns( order_by: 'tx_hash', order_dir: 'asc', 
+      start_block: 280537, end_block: 280539 )}
+    its('length'){ should eq(10) }
+    its('first') do
+      should eq(Counterparty::Burn.new( tx_index: 1096, 
+      source: '1ADpYypUcnbezuuYpCyRCY7G4KD6a9YXiF', block_index: 280537, 
+        earned: 129754545455, status: "valid", burned: 100000000, 
+        tx_hash: '6e905ec73870d6c6cdc8f4e64767ec41f74c8f07a24cc7c54811134f5b6aa6a7' 
+      ) )
     end
-    #subject{@cp.get_burns( order_by: 'tx_hash', order_dir: 'asc', 
-      #start_block: 280537, end_block: 280539 )}
-
   end
   
 end
