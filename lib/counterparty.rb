54c6c0e38aac01ce1869d8aa701e9433fb0c731f
Chris DeRose <cderose@lemmy.derosetechnologies.com>
2014-12-13 01:03:46 +0000

    Getting the requests going...

--- lib/counterparty.rb
+++ lib/counterparty.rb
@@ -1,11 +1,33 @@
 require 'counterparty/version'
+require 'json'
+require 'rest_client'
 
 module Counterparty
   class Client
     attr_accessor :host, :port, :username, :password
 
     def initialize(host='localhost', port=4000, username='rpc', password='1234')
-      @host,@port,@username,@password=host,port,username,password
+      @host,@port,@username,@password=host.to_s,port.to_i,username.to_s,password.to_s
     end
+
+    def get_burns(params)
+      request 'get_burns', params
+    end
+
+    def api_url
+      ['http://',@host,':',@port.to_s,'/api/'].join
+    end
+
+    private
+
+    def request(method, params)
+      JSON.parse RestClient::Request.execute( method: :post, url: api_url,
+        user: @username, password: @password,
+        headers: { accept: 'json', content_type: 'json' },
+        data: { method: method, params: params, jsonrpc: '2.0', 
+          id: '0' }.to_json 
+      ).to_str
+    end
+
   end
 end
--- spec/counterparty_client_spec.rb
+++ spec/counterparty_client_spec.rb
@@ -6,15 +6,21 @@ describe Counterparty::Client do
   describe "#new" do
     subject{default_cp}
 
-    its(:host){ should be('localhost') }
-    its(:port){ should be(4000) }
-    its(:username){ should be('rpc') }
-    its(:password){ should be('1234') }
+    its(:host){ should eq('localhost') }
+    its(:port){ should eq(4000) }
+    its(:username){ should eq('rpc') }
+    its(:password){ should eq('1234') }
+    its(:api_url){ should eq('http://localhost:4000/api/') }
   end
 
   describe "#get_burns" do
-    subject{@cp.get_burns( order_by: 'tx_hash', order_dir: 'asc', 
-      start_block: 280537, end_block: 280539 )}
+    it "what happens" do
+      ret = default_cp.get_burns( order_by: 'tx_hash', order_dir: 'asc', 
+        start_block: 280537, end_block: 280539 )
+      puts ret.inspect
+    end
+    #subject{@cp.get_burns( order_by: 'tx_hash', order_dir: 'asc', 
+      #start_block: 280537, end_block: 280539 )}
 
   end
   
