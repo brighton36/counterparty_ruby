adc6d72c0c98b63899708a9a426b552e8f6d1816
Chris DeRose <chris@chrisderose.com>
2015-05-31 22:26:51 +0000

    Adjusted the module declarations

--- lib/counterparty/tx_decode.rb
+++ lib/counterparty/tx_decode.rb
@@ -2,116 +2,118 @@
 
 # Inspriration heavily lifted from: 
 # - https://github.com/tokenly/counterparty-transaction-parser
-class Counterparty::TxDecode
-  class UndefinedBehavior < StandardError; end # If you find one of these, lmk!
-  class InvalidOpReturn < StandardError; end
-  class InvalidOutput < StandardError; end
-  class MultisigUnsupported < StandardError; end
+module Counterparty
+  class TxDecode
+    class UndefinedBehavior < StandardError; end # If you find one of these, lmk!
+    class InvalidOpReturn < StandardError; end
+    class InvalidOutput < StandardError; end
+    class MultisigUnsupported < StandardError; end
 
-  DEFAULT_PREFIX = 'CNTRPRTY'
+    DEFAULT_PREFIX = 'CNTRPRTY'
 
-  OP_RETURN_PARTS = /^OP_RETURN ([a-z0-9]+)$/
-  P2PKH_PARTS = /^OP_DUP OP_HASH160 ([a-z0-9]+) OP_EQUALVERIFY OP_CHECKSIG$/
-  OP_MULTISIG_PARTS = /^[12] ([a-z0-9 ]+) ([23]) OP_CHECKMULTISIG$/
+    OP_RETURN_PARTS = /^OP_RETURN ([a-z0-9]+)$/
+    P2PKH_PARTS = /^OP_DUP OP_HASH160 ([a-z0-9]+) OP_EQUALVERIFY OP_CHECKSIG$/
+    OP_MULTISIG_PARTS = /^[12] ([a-z0-9 ]+) ([23]) OP_CHECKMULTISIG$/
 
-  attr_accessor :sender_addr, :receiver_addr, :decrypt_key, :data, :prefix
+    attr_accessor :sender_addr, :receiver_addr, :decrypt_key, :data, :prefix
 
-  def initialize(tx, options = {})
-    @prefix = options[:prefix] || DEFAULT_PREFIX
-    @decrypt_key = options[:decrypt_key] || [tx.inputs[0].prev_out.reverse_hth].pack('H*')
-    
-    parse! (tx.respond_to? :outputs) ? 
-      tx.outputs.collect{|out| out.parsed_script.to_string } : tx
-  end
+    def initialize(tx, options = {})
+      @prefix = options[:prefix] || DEFAULT_PREFIX
+      @decrypt_key = options[:decrypt_key] || [tx.inputs[0].prev_out.reverse_hth].pack('H*')
+      
+      parse! (tx.respond_to? :outputs) ? 
+        tx.outputs.collect{|out| out.parsed_script.to_string } : tx
+    end
 
-  def decrypt(chunk)
-    RC4.new(decrypt_key).decrypt chunk
-  end
+    def decrypt(chunk)
+      RC4.new(decrypt_key).decrypt chunk
+    end
 
-  private
+    private
 
-  def prefixed?(data, offset = 1)
-    data[offset...(prefix.length+offset)] == prefix
-  end
+    def prefixed?(data, offset = 1)
+      data[offset...(prefix.length+offset)] == prefix
+    end
 
-  # This determines if the supplied operation is a non-data 
-  def is_non_prefixed_p2pkh?(op)
-    P2PKH_PARTS.match(op) && !prefixed?(decrypt([$1].pack('H*')))
-  end
+    # This determines if the supplied operation is a non-data 
+    def is_non_prefixed_p2pkh?(op)
+      P2PKH_PARTS.match(op) && !prefixed?(decrypt([$1].pack('H*')))
+    end
 
-  def hash160_from_p2pkh(op)
-    Bitcoin.hash160_to_address $1 if P2PKH_PARTS.match op
-  end
+    def hash160_from_p2pkh(op)
+      Bitcoin.hash160_to_address $1 if P2PKH_PARTS.match op
+    end
 
-  # Note that this isn't exactly the way counterparty parses the transactions. 
-  # But , it reads cleaner, and worked on every transaction I could find. 
-  # Mostly the difference is that the counterparty version is a loop that 
-  # collects P2PKH's anywhere in the outputs, whereas this block takes a sender
-  # or receiver address from the top and bottom only
-  def parse!(outputs)
-    return if @data
-    
-    # So typically, the first output is a P2PKH with the receiver address:
-    @receiver_addr = hash160_from_p2pkh outputs.shift if is_non_prefixed_p2pkh? outputs.first
+    # Note that this isn't exactly the way counterparty parses the transactions. 
+    # But , it reads cleaner, and worked on every transaction I could find. 
+    # Mostly the difference is that the counterparty version is a loop that 
+    # collects P2PKH's anywhere in the outputs, whereas this block takes a sender
+    # or receiver address from the top and bottom only
+    def parse!(outputs)
+      return if @data
+      
+      # So typically, the first output is a P2PKH with the receiver address:
+      @receiver_addr = hash160_from_p2pkh outputs.shift if is_non_prefixed_p2pkh? outputs.first
 
-    # Parse the sender address:
-    @sender_addr = hash160_from_p2pkh outputs.pop if is_non_prefixed_p2pkh? outputs.last
+      # Parse the sender address:
+      @sender_addr = hash160_from_p2pkh outputs.pop if is_non_prefixed_p2pkh? outputs.last
 
-    @data = outputs.collect{|out|
-      # This weirdo line will send the regex matches to the operation handler
-      # that matches the line
-      send (case out
-        when OP_RETURN_PARTS then :data_from_opreturn
-        when P2PKH_PARTS then :data_from_opchecksig
-        when OP_MULTISIG_PARTS then :data_from_opcheckmultisig
-        else raise InvalidOutput
-      end), *$~.to_a[1..-1]
-    }.compact.join
-  end
+      @data = outputs.collect{|out|
+        # This weirdo line will send the regex matches to the operation handler
+        # that matches the line
+        send (case out
+          when OP_RETURN_PARTS then :data_from_opreturn
+          when P2PKH_PARTS then :data_from_opchecksig
+          when OP_MULTISIG_PARTS then :data_from_opcheckmultisig
+          else raise InvalidOutput
+        end), *$~.to_a[1..-1]
+      }.compact.join
+    end
 
-  def data_from_opreturn(data)
-    chunk = [data].pack('H*')
-    
-    raise InvalidOpReturn if chunk.nil?
+    def data_from_opreturn(data)
+      chunk = [data].pack('H*')
+      
+      raise InvalidOpReturn if chunk.nil?
 
-    data = decrypt chunk
+      data = decrypt chunk
 
-    raise InvalidOpReturn unless prefixed?(data,0)
+      raise InvalidOpReturn unless prefixed?(data,0)
 
-    data[prefix.length..-1]
-  end
+      data[prefix.length..-1]
+    end
 
-  def data_from_opchecksig(pubkeyhash_text)
-    chunk = decrypt [pubkeyhash_text].pack('H*')
+    def data_from_opchecksig(pubkeyhash_text)
+      chunk = decrypt [pubkeyhash_text].pack('H*')
 
-    # This should get picked up by the sender and receiver pop/shift in parse!
-    # However, the counterparty lib would parse these obtuse addresses typically
-    # ATM - I can't find one in the wild
-    raise UndefinedBehavior unless prefixed? chunk
-    
-    chunk_length = chunk[0].ord
-    chunk[(1+prefix.length)...chunk_length+1]
-  end
+      # This should get picked up by the sender and receiver pop/shift in parse!
+      # However, the counterparty lib would parse these obtuse addresses typically
+      # ATM - I can't find one in the wild
+      raise UndefinedBehavior unless prefixed? chunk
+      
+      chunk_length = chunk[0].ord
+      chunk[(1+prefix.length)...chunk_length+1]
+    end
 
-  def data_from_opcheckmultisig(pubkeys_op, n_signatures_op)
-    pubkeys, n_signatures = pubkeys_op.split(' '), n_signatures_op.to_i
+    def data_from_opcheckmultisig(pubkeys_op, n_signatures_op)
+      pubkeys, n_signatures = pubkeys_op.split(' '), n_signatures_op.to_i
 
-    # There's no data in the last pubkey:
-    chunk = pubkeys[0...-1].collect{ |pubkey|
-      [pubkey].pack('H*')[1...-1] # Skip sign byte and nonce byte.
-    }.reduce(:+)
+      # There's no data in the last pubkey:
+      chunk = pubkeys[0...-1].collect{ |pubkey|
+        [pubkey].pack('H*')[1...-1] # Skip sign byte and nonce byte.
+      }.reduce(:+)
 
-    data = decrypt chunk
+      data = decrypt chunk
 
-    # Multisig transactions will break here. I haven't actually collected enough
-    # of these samples yet to really test, but it seems they are typically are
-    # at the top of the output stack:
-    raise MultisigUnsupported unless prefixed? data
+      # Multisig transactions will break here. I haven't actually collected enough
+      # of these samples yet to really test, but it seems they are typically are
+      # at the top of the output stack:
+      raise MultisigUnsupported unless prefixed? data
 
-    # Padding byte in each output (instead of just in the last one) so that 
-    # encoding methods may be mixed. Also, it’s just not very much data.
-    chunk_length = data[0].ord
+      # Padding byte in each output (instead of just in the last one) so that 
+      # encoding methods may be mixed. Also, it’s just not very much data.
+      chunk_length = data[0].ord
 
-    data[1..chunk_length][prefix.length..-1]
+      data[1..chunk_length][prefix.length..-1]
+    end
   end
 end
--- lib/counterparty/tx_encode.rb
+++ lib/counterparty/tx_encode.rb
@@ -2,132 +2,134 @@
 
 # Inspriration heavily lifted from: 
 # - https://github.com/CounterpartyXCP/counterparty-lib/blob/master/counterpartylib/lib/transaction.py
-class Counterparty::TxEncode
-  class MissingPubkey < StandardError; end
-  class MissingSenderAddr < StandardError; end
-  class InvalidPubkey < StandardError; end
-  class InvalidPubkeyGenerated < StandardError; end
-  class DataTooLarge < StandardError; end
+module Counterparty
+  class TxEncode
+    class MissingPubkey < StandardError; end
+    class MissingSenderAddr < StandardError; end
+    class InvalidPubkey < StandardError; end
+    class InvalidPubkeyGenerated < StandardError; end
+    class DataTooLarge < StandardError; end
 
-  PREFIX = 'CNTRPRTY'
+    PREFIX = 'CNTRPRTY'
 
-  P2PKH = 'OP_DUP OP_HASH160 %s OP_EQUALVERIFY OP_CHECKSIG'
-  MULTISIG = '1 %s %s OP_CHECKMULTISIG'
-  OPRETURN = 'OP_RETURN %s'
+    P2PKH = 'OP_DUP OP_HASH160 %s OP_EQUALVERIFY OP_CHECKSIG'
+    MULTISIG = '1 %s %s OP_CHECKMULTISIG'
+    OPRETURN = 'OP_RETURN %s'
 
-  # 33 is the size of a pubkey, there are two pubkeys in a multisig, 1 byte
-  # is lost for the data length byte, and two bytes are lost on each key for
-  # the data_to_pubkey inefficiency
-  BYTES_IN_MULTISIG = (33 * 2) - 1 - 2 - 2
-  BYTES_IN_PUBKEYHASH = 20 - 1
-  BYTES_IN_OPRETURN = 40
+    # 33 is the size of a pubkey, there are two pubkeys in a multisig, 1 byte
+    # is lost for the data length byte, and two bytes are lost on each key for
+    # the data_to_pubkey inefficiency
+    BYTES_IN_MULTISIG = (33 * 2) - 1 - 2 - 2
+    BYTES_IN_PUBKEYHASH = 20 - 1
+    BYTES_IN_OPRETURN = 40
 
-  attr_accessor :sender_addr, :sender_pubkey, :receiver_addr, :encrypt_key, 
-    :source_data, :prefix
-  
-  def initialize(encrypt_key, source_data, options = {})
-    @source_data, @encrypt_key = source_data, encrypt_key
+    attr_accessor :sender_addr, :sender_pubkey, :receiver_addr, :encrypt_key, 
+      :source_data, :prefix
+    
+    def initialize(encrypt_key, source_data, options = {})
+      @source_data, @encrypt_key = source_data, encrypt_key
 
-    if options[:sender_pubkey]
-      @sender_pubkey = options[:sender_pubkey]
-      @sender_addr = Bitcoin.pubkey_to_address(sender_pubkey)
-    elsif options.has_key? :sender_addr
-      @sender_addr = options[:sender_addr]
-    else
-      raise MissingSenderAddr
+      if options[:sender_pubkey]
+        @sender_pubkey = options[:sender_pubkey]
+        @sender_addr = Bitcoin.pubkey_to_address(sender_pubkey)
+      elsif options.has_key? :sender_addr
+        @sender_addr = options[:sender_addr]
+      else
+        raise MissingSenderAddr
+      end
+
+      @receiver_addr = options[:receiver_addr] if options.has_key? :receiver_addr
+
+      @prefix = options[:prefix] || PREFIX
     end
 
-    @receiver_addr = options[:receiver_addr] if options.has_key? :receiver_addr
+    def to_opmultisig
+      raise MissingPubkey unless sender_pubkey
 
-    @prefix = options[:prefix] || PREFIX
-  end
+      data_length = BYTES_IN_MULTISIG-prefix.length
+      p2pkh_wrap collect_chunks(source_data,data_length){|chunk| 
+        padding = 0.chr * (data_length-chunk.length)
 
-  def to_opmultisig
-    raise MissingPubkey unless sender_pubkey
+        data = encrypt [(chunk.length+prefix.length).chr,prefix, chunk, padding].join
 
-    data_length = BYTES_IN_MULTISIG-prefix.length
-    p2pkh_wrap collect_chunks(source_data,data_length){|chunk| 
-      padding = 0.chr * (data_length-chunk.length)
+        data_keys = [(0...31), (31...62)].collect{|r| data_to_pubkey data[r] }
 
-      data = encrypt [(chunk.length+prefix.length).chr,prefix, chunk, padding].join
+        MULTISIG % [(data_keys + [sender_pubkey]).join(' '), 3]
+      }
+    end
 
-      data_keys = [(0...31), (31...62)].collect{|r| data_to_pubkey data[r] }
+    def to_pubkeyhash
+      p2pkh_wrap collect_chunks(source_data, BYTES_IN_PUBKEYHASH-prefix.length){ |chunk|
+        data_length = prefix.length + chunk.length
 
-      MULTISIG % [(data_keys + [sender_pubkey]).join(' '), 3]
-    }
-  end
+        padding = 0.chr * (BYTES_IN_PUBKEYHASH - data_length)
+    
+        enc_chunk = encrypt [(data_length).chr, prefix, chunk, padding].join
 
-  def to_pubkeyhash
-    p2pkh_wrap collect_chunks(source_data, BYTES_IN_PUBKEYHASH-prefix.length){ |chunk|
-      data_length = prefix.length + chunk.length
+        P2PKH % enc_chunk.unpack('H*').first
+      }
+    end
 
-      padding = 0.chr * (BYTES_IN_PUBKEYHASH - data_length)
-  
-      enc_chunk = encrypt [(data_length).chr, prefix, chunk, padding].join
+    def to_opreturn
+      # I'm fairly certain that using more than one OP_RETURN per transaction is
+      # unstandard behavior right now
+      raise DataTooLarge if (source_data.length + prefix.length) > BYTES_IN_OPRETURN
 
-      P2PKH % enc_chunk.unpack('H*').first
-    }
-  end
+      data = encrypt [prefix,source_data].join
 
-  def to_opreturn
-    # I'm fairly certain that using more than one OP_RETURN per transaction is
-    # unstandard behavior right now
-    raise DataTooLarge if (source_data.length + prefix.length) > BYTES_IN_OPRETURN
+      p2pkh_wrap( OPRETURN % data.unpack('H*').first )
+    end
 
-    data = encrypt [prefix,source_data].join
+    def encrypt(chunk)
+      RC4.new(encrypt_key).encrypt chunk
+    end
 
-    p2pkh_wrap( OPRETURN % data.unpack('H*').first )
-  end
+    private
 
-  def encrypt(chunk)
-    RC4.new(encrypt_key).encrypt chunk
-  end
+    def p2pkh_wrap(operation)
+      [ (receiver_addr) ? P2PKH % Bitcoin.hash160_from_address(receiver_addr) : nil, 
+        operation,
+        P2PKH % Bitcoin.hash160_from_address(sender_addr) ].flatten.compact
+    end
 
-  private
+    # Take a too short data pubkey and make it look like a real pubkey.
+    # Take an obfuscated chunk of data that is two bytes too short to be a pubkey and
+    # add a sign byte to its beginning and a nonce byte to its end. Choose these
+    # bytes so that the resulting sequence of bytes is a fully valid pubkey (i.e. on
+    # the ECDSA curve). Find the correct bytes by guessing randomly until the check
+    # passes. (In parsing, these two bytes are ignored.)
+    #
+    # NOTE: This function is named "make_fully_valid" in the official code. 
+    def data_to_pubkey(bytes)
+      raise InvalidPubkey unless bytes.length == 31
 
-  def p2pkh_wrap(operation)
-    [ (receiver_addr) ? P2PKH % Bitcoin.hash160_from_address(receiver_addr) : nil, 
-      operation,
-      P2PKH % Bitcoin.hash160_from_address(sender_addr) ].flatten.compact
-  end
+      random_bytes = Digest::SHA256.digest bytes
 
-  # Take a too short data pubkey and make it look like a real pubkey.
-  # Take an obfuscated chunk of data that is two bytes too short to be a pubkey and
-  # add a sign byte to its beginning and a nonce byte to its end. Choose these
-  # bytes so that the resulting sequence of bytes is a fully valid pubkey (i.e. on
-  # the ECDSA curve). Find the correct bytes by guessing randomly until the check
-  # passes. (In parsing, these two bytes are ignored.)
-  #
-  # NOTE: This function is named "make_fully_valid" in the official code. 
-  def data_to_pubkey(bytes)
-    raise InvalidPubkey unless bytes.length == 31
+      # Deterministically generated, for unit tests.
+      sign = (random_bytes[0].ord & 1) + 2
+      nonce = initial_nonce = random_bytes[1].ord
 
-    random_bytes = Digest::SHA256.digest bytes
+      begin
+        nonce += 1
+        next if nonce == initial_nonce
 
-    # Deterministically generated, for unit tests.
-    sign = (random_bytes[0].ord & 1) + 2
-    nonce = initial_nonce = random_bytes[1].ord
+        ret = (sign.chr + bytes + (nonce % 256).chr).unpack('H*').first
 
-    begin
-      nonce += 1
-      next if nonce == initial_nonce
+      # Note that 256 is the exclusive limit:
+      end until Bitcoin.valid_pubkey? ret
 
-      ret = (sign.chr + bytes + (nonce % 256).chr).unpack('H*').first
+      # I don't actually think this is ever possible. Note that we return 66 bytes
+      # as this is string of hex, and not the bytes themselves:
+      raise InvalidPubkeyGenerated unless ret.length == 66
 
-    # Note that 256 is the exclusive limit:
-    end until Bitcoin.valid_pubkey? ret
+      ret
+    end
 
-    # I don't actually think this is ever possible. Note that we return 66 bytes
-    # as this is string of hex, and not the bytes themselves:
-    raise InvalidPubkeyGenerated unless ret.length == 66
-
-    ret
-  end
-
-  # This is a little helper method that lets us split our binary data into 
-  # chunks for further processing
-  def collect_chunks(data,chunk_length, &block)
-    (source_data.length.to_f / chunk_length).ceil.times.collect{|i| 
-      block.call source_data.slice(i*chunk_length, chunk_length) }
+    # This is a little helper method that lets us split our binary data into 
+    # chunks for further processing
+    def collect_chunks(data,chunk_length, &block)
+      (source_data.length.to_f / chunk_length).ceil.times.collect{|i| 
+        block.call source_data.slice(i*chunk_length, chunk_length) }
+    end
   end
 end
